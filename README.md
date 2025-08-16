fc0c496a7b0a7ff962db0eedb93d41b7a64753fe
ClaudioLinimitabile <damicoclaudio71096@gmail.com>
2025-08-16 14:52:22 +0200

    added description and an how to use paragraph

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TurnBasedSistem
 A very simple turn based sistem to implement in godot 4 project 
+
+HOW TO USE:
+-for every node that need to be managed by turn use NewEntity(name,flag) by default the flag is false you have to define an entity that is true to put it as a starter entity (the first to move) be ware that only one entity can be a starter entity
+    EXAMPLE: NewEntity(self.name,true) 
+-you can shift the turn in the entity list by using ChangeTurn(Name) it is going to revers the turn of the entity selected and making true the next entity on the list
+-you can listen in a _process() for the turn for a particular entity to become true whit RequestTurn(Name) it is going to return true if the entity selected has turn true
+    EXAMPLE: if RequestTurn(self.name):
+                #your logic here
--- TurnManager.gd
+++ TurnManager.gd
@@ -4,6 +4,7 @@ var ENTITY = {}
 var index: int = 0
 var TopEntity
 
+#create a new entity to the manager
 func NewEntity(Name: String, StarterFlag: bool = false) ->void:
 	if StarterFlag:
 		assert(TopEntity == null, "Error: more than 1 entity assigned as a start entity")
@@ -12,6 +13,7 @@ func NewEntity(Name: String, StarterFlag: bool = false) ->void:
 	print("🔄 [TurnManager]: added entity: "+Name+" whit Flag: "+str(StarterFlag))
 	index += 1
 
+#reverse the turn of the selected name and also reverse the turn of the next entity(that can be the first if the list is finished)
 func ChangeTurn(Name: String):
 	for x in ENTITY:
 		if ENTITY[x][0]==Name:
@@ -21,6 +23,7 @@ func ChangeTurn(Name: String):
 			else:
 				ENTITY[0][1]= true
 
+#return true if the entity that is requesting have turn true
 func RequestTurn(Name: String) -> bool:
 	assert(TopEntity != null, "Error: no starter entity assigned")
 	for x in ENTITY:
